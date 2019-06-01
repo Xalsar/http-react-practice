8f1287a106c54f114015b7d52a59d3c9e8d33749
Xavier Carrera Gimbert <xalsar@protonmail.com>
2019-06-01 11:21:11 +0200

    219. Restructure files to be able to handle routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,62 +1,29 @@
 import React, { Component } from 'react';
 // import axios from 'axios'
+import { Route } from 'react-router-dom'
 import axios from '../../axios'
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
+import NewPost from './NewPost/NewPost';
+import Posts from './Posts/Posts'
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    async componentDidMount() {
-        try {
-            const response = await axios.get('/posts')
-            const posts = response.data.slice(0, 4)
-            const updatedPosts = posts.map(post => ({
-                ...post,
-                author: 'Max'
-            }))
-            this.setState({ posts: updatedPosts })
-        } catch (error) {
-
-        }
-    }
-
-    postSelectedhandler = (id) => { this.setState({ selectedPostId: id }) }
-
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>Somethink when wrong</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => (
-                <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedhandler(post.id)} />
-            ))
-        }
-
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
                             <li><a href="/">Home</a></li>
-                            <li><a href="/">New Post</a></li>
+                            <li><a href="/post">New Post</a></li>
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <Route path="/" exact render={() => <h1>Home</h1>}/>
+                <Posts/>
                 <section>
-                    <FullPost id={this.state.selectedPostId} />
+                    <FullPost/>
                 </section>
                 <section>
                     <NewPost />
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import axios from '../../../axios'
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios.get('/posts')
+            const posts = response.data.slice(0, 4)
+            const updatedPosts = posts.map(post => ({
+                ...post,
+                author: 'Max'
+            }))
+            this.setState({ posts: updatedPosts })
+        } catch (error) {
+
+        }
+    }
+
+    postSelectedhandler = (id) => { this.setState({ selectedPostId: id }) }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Somethink when wrong</p>
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => (
+                <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedhandler(post.id)} />
+            ))
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts
